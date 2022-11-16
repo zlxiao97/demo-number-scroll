2999f44a7f73b3795757a09c0a6752fdd36a96bf
肖子立 <xiaozlb@digitalchina.com>
2022-11-16 13:42:28 +0800

    step5: 让数字列表停止滚动

--- src/components/NumberScroll/NumberScrollItem.js
+++ src/components/NumberScroll/NumberScrollItem.js
@@ -3,14 +3,16 @@ import { v4 as uuidV4 } from 'uuid'
 import useNumberScroll from './hooks/useNumberScroll'
 
 const NumberScrollItem = (props) => {
-  const ref = useNumberScroll()
+  const ref = useNumberScroll({
+    duration: 5,
+  })
   const numbers = new Array(10)
     .fill(null)
     .map((_, index) => index)
     .concat(0)
   return (
     <div className='number-scroll__stage' ref={ref}>
-      <div className='number-scroll__gimmick '>
+      <div className='number-scroll__gimmick' data-value={0}>
         {numbers.map((number) => (
           <div key={uuidV4()}>{number}</div>
         ))}
--- src/components/NumberScroll/hooks/useNumberScroll.js
+++ src/components/NumberScroll/hooks/useNumberScroll.js
@@ -1,14 +1,43 @@
 import { useRef } from 'react'
 import gsap from 'gsap'
+import { min } from 'lodash'
 import useGSAP from './useGSAP'
 import { getDuration, getScrollSpeed } from '../../../utils/utils'
 
 const COUNT_OF_NUMBERS = 10 // 隐藏在舞台外的数字个数
 const HEIGHT_PER_NUMBER = 32 // 每个数字所占高度
 const ONE_LAP = HEIGHT_PER_NUMBER * COUNT_OF_NUMBERS // 列表滚动一圈的位移
+const ENDING_ANIMATION_PERCENT = 0.25 // 收尾动画的时间占比
 
-const useNumberScroll = (dependencies = []) => {
+const useNumberScroll = ({ duration }, dependencies = []) => {
   const ref = useRef()
+  const startTime = useRef()
+  const endingDuration = min([duration * ENDING_ANIMATION_PERCENT, 2]) // 结束动画最多播放 2 秒
+  const loopDuration = duration - endingDuration
+
+  const endAnimate = (ctx) => {
+    startTime.current = Number.POSITIVE_INFINITY
+    ctx.pause()
+    const ele = ctx.targets()[0]
+    const value = +ele.dataset.value
+    const valueOffset = value === 0 ? 10 : value
+    gsap.set(ele, {
+      css: {
+        filter: '',
+      },
+    })
+    gsap.fromTo(
+      ele,
+      {
+        y: -((valueOffset - 1) * HEIGHT_PER_NUMBER), // 目标数字的上一个数字
+      },
+      {
+        y: -(valueOffset * HEIGHT_PER_NUMBER),
+        duration: endingDuration,
+        ease: 'elastic.out(1, 0.3)',
+      }
+    )
+  }
 
   const animate = () => {
     const gimmicSelector = '.number-scroll__gimmick'
@@ -21,12 +50,17 @@ const useNumberScroll = (dependencies = []) => {
         ease: 'none',
         repeat: -1, // 无限重复
         onStart() {
+          startTime.current = Date.now()
           // 添加模糊滤镜
           gsap.set(gimmicSelector, {
             css: {
               filter: 'blur(1px)',
             },
           })
+        },
+        onUpdate() {
+          const isEnd = Date.now() - startTime.current >= loopDuration * 1000
+          if (isEnd) endAnimate(this)
         },
       }
     )
