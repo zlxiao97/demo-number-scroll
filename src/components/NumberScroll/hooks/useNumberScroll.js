388a7253d1fa2a55f38d333f0e2ebc98f9206e39
肖子立 <xiaozlb@digitalchina.com>
2022-11-16 11:23:03 +0800

    step4: 给滚动中的数字加上一层模糊滤镜

--- src/components/NumberScroll/hooks/useNumberScroll.js
+++ src/components/NumberScroll/hooks/useNumberScroll.js
@@ -1,6 +1,7 @@
-import { useLayoutEffect, useRef } from 'react'
+import { useRef } from 'react'
 import gsap from 'gsap'
 import useGSAP from './useGSAP'
+import { getDuration, getScrollSpeed } from '../../../utils/utils'
 
 const COUNT_OF_NUMBERS = 10 // 隐藏在舞台外的数字个数
 const HEIGHT_PER_NUMBER = 32 // 每个数字所占高度
@@ -16,9 +17,17 @@ const useNumberScroll = (dependencies = []) => {
       { y: 0 },
       {
         y: -ONE_LAP,
-        duration: 3,
+        duration: getDuration(ONE_LAP, getScrollSpeed(HEIGHT_PER_NUMBER)),
         ease: 'none',
         repeat: -1, // 无限重复
+        onStart() {
+          // 添加模糊滤镜
+          gsap.set(gimmicSelector, {
+            css: {
+              filter: 'blur(1px)',
+            },
+          })
+        },
       }
     )
   }
---
+++ src/utils/utils.js
@@ -0,0 +1,19 @@
+/**
+ * 关于滚动速度的计算：
+ * 1. 假设一个数字从进场到离场需要3帧
+ * 2. 从 24FPS / 30FPS / 60FPS 这几个常见帧率中选择一个作为平均运动速度
+ * 3. 每个数字的高度 = 字体大小 * 行高
+ * 4. 一帧需要运动的距离 = 数字的高度 / 从进场到离场需要的帧数
+ * 5. 滚动速度 = 一帧需要运动的距离 / 一帧在指定 FPS 下对应的时间
+ */
+const getTime = (fps) => 1 / fps
+
+export const getScrollSpeed = (heightPerNumber, FPS = 30) => {
+  const FRAME_PER_NUMER = 3
+  const heightPerFrame = heightPerNumber / FRAME_PER_NUMER
+  const timePerFrame = getTime(FPS)
+
+  return heightPerFrame / timePerFrame
+}
+
+export const getDuration = (s, v) => s / v
